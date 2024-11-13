aa5c6063984fdba2b80c0f8375591d5548c7e646
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-11-13 18:19:51 -0500

    working on chapter 12 practice

--- ThinkJavaChapter12Practice/Card.java
+++ ThinkJavaChapter12Practice/Card.java
@@ -4,13 +4,22 @@ public class Card {
     private int rank;
     private int suit;
 
+
     public Card(int rank, int suit) {
         this.rank = rank;
         this.suit = suit;
     }
-    Card threeOfClubs = new Card(3, 0);
 
-    String[] suits = new String{"Clubs", "Diamonds", "Hearts", "Spades"};
-    String[] ranks = new String{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public String toString() {
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String s = ranks[this.rank] + " of " + suits[this.suit];
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Card card = new Card(11, 2);
+        System.out.println(card);
+    }
 
 }
